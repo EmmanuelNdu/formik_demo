12a56ada7b740721031f6dbc4b9bcab44aceea8d
Ndubuaku Emmanuel <ndubuaku.emmanuel@gmail.com>
2025-01-02 01:01:25 +0100

    error

--- src/components/YoutubeForm.jsx
+++ src/components/YoutubeForm.jsx
@@ -1,16 +1,13 @@
-
-
-
-
-
 import React from 'react'
-import { useFormik } from 'formik'
+import { Formik, Form, Field, ErrorMessage } from 'formik'
 import * as Yup from 'yup'
 
 const initialValues =  {
     name:"",
     email:"",
-    channel:""
+    channel:"",
+    comment:"",
+    address:""
 }
 
 const onSubmit = values => {
@@ -26,58 +23,59 @@ const validationSchema = Yup.object({
 })
 
 const YoutubeForm = () => {
-
-   const formik  = useFormik({
-    initialValues,
-    onSubmit,
-    validationSchema
-    // validate
-   })
-
-   console.log('visited fields', formik.touched)
-
   return (
-    <div>
-        <form onSubmit={formik.handleSubmit}>
+    <Formik
+    initialValues={initialValues}
+    validationSchema={validationSchema}
+    onSubmit={onSubmit}>
+        <Form>
             <div className='form-control'>
             <label htmlFor='name'>Name</label>
-            <input type='text' 
-            id='name'
-            name='name' 
-           {... formik.getFieldProps('name')}
-            />
-            { formik.touched.name && formik.errors.name ? 
-            ( <div className='error' >{formik.errors.name}</div>
-            ) : null}
+            <Field type='text' id='name'name='name'/>
+          <ErrorMessage name='name' component='div'/>
             </div> 
 
             <div className='form-control'>
             <label htmlFor='email'>Email</label>
-            <input type='email' 
-            id='email' 
-            name='email' 
-            {... formik.getFieldProps('email')}
-            />
-            { formik.touched.email && formik.errors.email ? 
-            ( <div className='error'>{formik.errors.email}</div> 
-            ) : null}
+            <Field type='email'id='email'name='email'/>
+           <ErrorMessage name='email' />
             </div> 
 
             <div className='form-control'>
             <label htmlFor='channel'>Channel</label>
-            <input type='text' 
-            id='channel' 
-            name='channel' 
-            {... formik.getFieldProps('channel')}
+            <Field 
+            type='text'
+            id='channel'
+            name='channel'
+            placeholder='Youtube channel name'
             />
-            {formik.touched.channel && formik.errors.channel ? 
-            (<div className='error' >{formik.errors.channel}</div>
-            ) : null}
-            </div> 
+           <ErrorMessage name='channel' />
+            </div>
+
+            <div className='form-control'>
+                <label htmlFor='comments' >comments</label>
+                <Field as ='textarea' id='comments' name='comments'/>
+                </div> 
+
+                <div className='form-control'>
+                    <label htmlFor='address'>Address</label>
+                    <Field name='address'>
+                        {
+                            (props) => {
+                                const {field, form, meta} = props
+                                console.log('Render props', props)
+                                return<div>
+                                    <input type='text' id='address' { ...Field}/>
+                                    {meta.touched && meta.error ? <div>{meta.error}</div> : null}
+                                </div>
+                            }
+                        }
+                    </Field>
+                </div>
 
             <button type='submit'>Submit</button>
-        </form>
-    </div>
+        </Form>
+    </Formik>
   )
 }
 
